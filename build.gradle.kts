707c19caf8fc6373d15d95d13b27060bc20c1a17
Sir Wabbit <559086+sir-wabbit@users.noreply.github.com>
2026-03-07 23:24:08 -0500

    chore: regenerate project files
    Semver Impact: NONE

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,19 +3,17 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 repositories {
     mavenCentral()
-
-    maven("https://jitpack.io")
 }
 
 group   = "one.wabbit"
 version = "0.0.1"
 
 plugins {
-    kotlin("jvm") version "2.2.20"
-    id("org.jetbrains.dokka") version "2.0.0"
-    id("org.jetbrains.kotlinx.kover") version "0.9.1"
+    kotlin("jvm")
+    id("org.jetbrains.dokka")
+    id("org.jetbrains.kotlinx.kover")
 
-    kotlin("plugin.serialization") version "2.2.20"
+    kotlin("plugin.serialization")
 
     id("maven-publish")
 }
@@ -32,8 +30,8 @@ publishing {
 }
 
 dependencies {
-    implementation("one.wabbit:kotlin-pprint:1.0.0")
-    implementation("one.wabbit:kotlin-web-common:1.1.0")
+    implementation(project(":kotlin-pprint")) // 1.0.0
+    implementation(project(":kotlin-web-common")) // 1.1.0
 
     testImplementation(kotlin("test"))
 
@@ -70,7 +68,9 @@ tasks {
     withType<KotlinCompile> {
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_21)
-            freeCompilerArgs.add("-Xcontext-receivers")
+
+            freeCompilerArgs.add("-Xcontext-parameters")
+
         }
     }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,14 +2,52 @@ rootProject.name = "kotlin-web-scihub"
 
 dependencyResolutionManagement {
     repositories {
+        google()
         mavenCentral()
-
+        maven("https://repo.papermc.io/repository/maven-public/")
     }
 }
 
 pluginManagement {
     repositories {
+        google()
         gradlePluginPortal()
-
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+    plugins {
+        id("org.jetbrains.kotlin.jvm") version "2.3.10"
+        id("org.jetbrains.kotlin.js") version "2.3.10"
+        id("org.jetbrains.kotlin.multiplatform") version "2.3.10"
+        id("org.jetbrains.kotlin.plugin.serialization") version "2.3.10"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.3.10"
+        id("com.android.application") version "8.13.2"
+        id("com.android.library") version "8.13.2"
+        id("com.android.kotlin.multiplatform.library") version "8.13.2"
+        id("org.jetbrains.compose") version "1.9.1"
+        id("com.gradleup.shadow") version "8.3.0"
+        id("org.jetbrains.dokka") version "2.0.0"
+        id("org.jetbrains.kotlinx.kover") version "0.9.3"
+        id("org.jetbrains.intellij") version "1.17.2"
+        id("io.papermc.paperweight.userdev") version "1.7.2"
+        id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
     }
 }
+
+include(":kotlin-pprint")
+project(":kotlin-pprint").projectDir = file("../kotlin-pprint")
+include(":kotlin-web-common")
+project(":kotlin-web-common").projectDir = file("../kotlin-web-common")
+include(":kotlin-data-ref")
+project(":kotlin-data-ref").projectDir = file("../kotlin-data-ref")
+include(":kotlin-data-need")
+project(":kotlin-data-need").projectDir = file("../kotlin-data-need")
+include(":kotlin-java-escape")
+project(":kotlin-java-escape").projectDir = file("../kotlin-java-escape")
+include(":kotlin-doc")
+project(":kotlin-doc").projectDir = file("../kotlin-doc")
+include(":kotlin-random-gen")
+project(":kotlin-random-gen").projectDir = file("../kotlin-random-gen")
+include(":kotlin-data")
+project(":kotlin-data").projectDir = file("../kotlin-data")
+include(":kotlin-base58")
+project(":kotlin-base58").projectDir = file("../kotlin-base58")
